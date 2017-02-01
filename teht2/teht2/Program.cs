7c9b16e9a98f3efeabb0050080f05fc0ba60da7e
ella <ella@ELLA>
2017-02-01 19:11:30 +0200

    teht2

---
+++ teht2/teht2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Washer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //washer
+            Washer washer = new Washer();
+            washer.On = true;
+            washer.Duration = "45 minutes";
+            washer.Program = "cotton";
+
+            //print data
+            washer.PrintData();
+            Console.ReadLine();
+
+            //turn washer off
+            washer.TurnOn();
+            Console.ReadLine();
+            washer.TurnOff();
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ teht2/teht2/Washer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Washer
+{
+    class Washer
+    {
+        public string Program { get; set; }
+        public string Duration { get; set; }
+
+        public bool On { get; set; }
+
+
+        //turn on
+
+        public void TurnOn()
+        {
+            On = !On;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Washer is on: " + On);
+            Console.WriteLine("Program is: " + Program + "," + " and it takes: " + Duration);
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Turning washer off...");
+            Console.WriteLine("Washer is on: " + On);
+        }
+
+
+    }
+}
