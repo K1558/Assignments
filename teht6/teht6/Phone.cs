e25be21ddf1e59d9f48bb3ed48212a8658835e8d
ella <ella@ELLA>
2017-02-01 21:08:06 +0200

    teht6 phone

---
+++ teht6/teht6/Phone.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht6
+{
+    class Phone
+    {
+        //model, touch-screen,ring-tone, volume-setting 
+        public string Model { get; set; }
+        public bool OnSilent { get; set; }
+        public int Volume { get; set; }
+
+
+        //phone rings, volume settings
+        public void Ring()
+        {
+            if (Volume > 0)
+                Console.WriteLine("ring,ring, "+Model + " is ringing");
+            else
+                Console.WriteLine("...");
+
+        }
+
+        public void VolumeUp()
+        {
+            Volume++;
+        }
+
+        public void VolumeDown()
+        {
+            Volume--;
+        }
+
+        public void IsItOnSilent()
+        {
+
+            if (Volume == 0)
+                OnSilent = true;
+            else
+                OnSilent = false;
+
+            Console.WriteLine("Is you're phone " + Model+ " on silent: " +OnSilent);
+        }
+        public void PrintPhone()
+        {
+            Console.WriteLine("You're phone is: "+Model + " and the volume is: " +Volume );
+            
+        }
+
+    }
+}
---
+++ teht6/teht6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Iphone 
+            Phone iphone = new Phone();
+            iphone.Model = "iphone 6S";
+            iphone.Volume = 5;
+
+            Phone huawei = new Phone();
+            huawei.Model = "Honor 8";
+            huawei.Volume = 0;
+
+            iphone.PrintPhone();
+            Console.ReadLine();
+            huawei.PrintPhone();
+            Console.ReadLine();
+
+            iphone.Ring();
+            Console.ReadLine();
+
+            for (int i=0; i<5;i++)
+            {
+                iphone.VolumeDown();
+            }
+
+            iphone.PrintPhone();
+            Console.ReadLine();
+
+            iphone.IsItOnSilent();
+            Console.ReadLine();
+
+            huawei.VolumeUp();
+            Console.ReadLine();
+
+            huawei.Ring();
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
