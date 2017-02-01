90fba4a87d4b695f7c53a405c7bfe95cf806615e
ella <ella@ELLA>
2017-02-01 19:04:30 +0200

    teht4

---
+++ teht4/teht4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle bike = new Vehicle();
+            bike.Name = "bike";
+            bike.Speed = 20;
+            bike.Tyres = 2;
+
+            Vehicle car = new Vehicle();
+            car.Name = "car";
+            car.Speed = 45;
+            car.Tyres = 4;
+
+            Vehicle tricycle = new Vehicle();
+            tricycle.Name = "tricycle";
+            tricycle.Speed = 10;
+            tricycle.Tyres = 3;
+
+            bike.PrintData();
+            car.PrintData();
+            tricycle.PrintData();
+            Console.ReadLine();
+
+            bike.ToString();
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
---
+++ teht4/teht4/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Vehicle
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Vechile is " + Name);
+            Console.WriteLine("Speed is " + Speed);
+            Console.WriteLine("Vehicle has " + Tyres + " tyres");
+        }
+
+        public void ToString()
+        {
+            string str = Convert.ToString(Name + " " + Speed + " " + Tyres);
+            Console.WriteLine(str);
+        }
+    }
+}
