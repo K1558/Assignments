ae79fe4f4a1bd91166509d764d563ac560a9e7ab
ella <ella@ELLA>
2017-02-01 20:10:22 +0200

    teht5

---
+++ teht5/teht5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // one student
+            Student Ella = new Student();
+            Ella.Name = "Ella";
+            Ella.StudentID = "K1558";
+            Ella.Age = 20;
+            Ella.Class = "TTV15S1";
+            Ella.Courses = "Fysiikka";
+
+            //other student 
+            Student Kalle = new Student();
+            Kalle.Name = "Kalle";
+            Kalle.StudentID = "K2323";
+            Kalle.Age = 26;
+            Kalle.Class = "TTV15S4";
+            Kalle.Courses = "Tietoturvateknologiat";
+
+            //methods 
+            Ella.PrintData();
+            Console.ReadLine();
+
+            Ella.AddMatikka();
+            Ella.PrintData();
+            Console.ReadLine();
+
+            Kalle.PrintData();
+            Console.ReadLine();
+
+            Kalle.AddOhjelmointi();
+            Kalle.PrintData();
+            Console.ReadLine();
+
+            Ella.AddOhjelmointi();
+            Ella.PrintData();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ teht5/teht5/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht5
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string StudentID { get; set; }
+        public string Class { get; set; }
+        public string Courses { get; set; }
+
+        // hbd
+
+        public void Hbd()
+        {
+            Age++;
+        }
+
+        //dropping out 
+
+        public void Dropping()
+        {
+            Class = "Dropping out";
+        }
+        
+        public void AddMatikka()
+        {
+            string str = Convert.ToString(Courses + ", Matematiikka");
+            Courses = str;
+            
+        }
+
+        public void AddOhjelmointi()
+        {
+            string s = Convert.ToString(Courses + ", Ohjelmointi");
+            Courses = s;
+        }
+
+
+        public void PrintData()
+        {
+            Console.WriteLine("Student's name: "+Name+ " Age: " +Age+ " Student-ID: "+StudentID+ " Class: " +Class);
+            Console.WriteLine("Student is taking classes: "+Courses);
+        }
+
+    }
+}
