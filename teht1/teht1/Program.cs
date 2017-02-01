cd31a761f245f18815f944690bd5127c56ba328d
ella <ella@ELLA>
2017-02-01 19:08:55 +0200

    teht1

---
+++ teht1/teht1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //stove
+            Stove stove = new Stove();
+            stove.Temperature = 0;
+            stove.On = false;
+
+            //display data
+            stove.PrintData();
+            Console.ReadLine();
+
+            //turn on
+            stove.TurnOn();
+            Console.ReadLine();
+
+            //change temperature 
+            stove.MoreHeat();
+
+            //display data
+            stove.PrintData();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ teht1/teht1/Stove.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stove
+{
+    class Stove
+    {
+        //properties
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public bool On { get; set; }
+
+
+        //method to turn on/off
+
+        public void TurnOn()
+        {
+            On = !On;
+        }
+
+        public void MoreHeat()
+        {
+            Temperature += 5;
+        }
+
+        public void LessHeat()
+        {
+            Temperature -= 5;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Stove is on: " + On);
+            Console.WriteLine("Temperature is: " + Temperature);
+
+        }
+    }
+}
