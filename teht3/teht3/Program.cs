7c5662276d224425e925f768debbad3981f3bae2
ella <ella@ELLA>
2017-02-01 18:42:55 +0200

    teht3

---
+++ teht3/teht3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tv Tv1 = new Tv();
+            Tv1.Off = true;
+            Tv1.Channel = 1;
+            Tv1.Output = "Tv";
+
+            Tv Tv2 = new Tv();
+            Tv2.Off = true;
+            Tv2.Channel = 9;
+            Tv2.Output = "HDMI";
+
+            //print data
+            Tv1.PrintData();
+            Tv2.PrintData();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ teht3/teht3/Tv.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Tv
+    {
+        public int Channel { get; set; }
+        public string Output { get; set; }
+
+        public bool Off { get; set; }
+
+        //turn on
+
+        public void TurnOff()
+        {
+            Off = !Off;
+        }
+
+        public void ChangeChannel()
+        {
+            Channel++;
+
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("TV is on: " + Off);
+            Console.WriteLine("Channel is: " + Channel);
+            Console.WriteLine("Output is: " + Output);
+        }
+
+
+    }
+}
