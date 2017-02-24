5bce06af81a3763320ece44a3a29cd11a44f5250
ella <ella@ELLA>
2017-02-24 15:09:05 +0200

    teht 1 24.2

---
+++ teht1.2/teht1.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht1._2
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            
+          
+                        using (StreamWriter outputFile = new StreamWriter(mydocpath + @"\WriteLines.txt"))
+                        {
+                        for (int i = 0; i < 3; i++)
+                            try
+                            {
+                                Console.WriteLine("Write a line: ");
+                                string line = Console.ReadLine();
+
+                                outputFile.WriteLine(line);
+                            
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                                break;
+                            }
+                         
+                        }
+            
+         
+
+            try
+            {
+                string text = System.IO.File.ReadAllText(mydocpath+@"\WriteLines.txt");
+                System.Console.WriteLine("The content of the file: \n"+text);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+            }
+
+        }
+    }
+}
---
+++ writingtofile/writingtofile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace writingtofile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamWriter outputFile = null;
+            try
+            {
+                outputFile = new System.IO.StreamWriter(@"c:\test.file");
+                outputFile.WriteLine("Here is a sample text to file.");
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Some exception happened!");
+                Console.WriteLine(ex.Message); // Access to the path 'c:\test.file' is denied.
+            }
+            finally
+            {
+                // check for null because OpenWrite might have failed
+                if (outputFile != null)
+                {
+                    outputFile.Close();
+                }
+            }
+        }
+    }
+}
